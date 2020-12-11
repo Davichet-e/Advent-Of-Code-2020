b4b7d9c7485e1ab60f06c346c200da70f7fe2588
David Garcia <davidgmorillop@gmail.com>
2020-12-11 21:14:29 +0100

    Minor changes

--- src/day_11.rs
+++ src/day_11.rs
@@ -2,6 +2,8 @@ use std::{fs, io};
 
 #[allow(dead_code)]
 pub fn day_11() -> io::Result<()> {
+    use Direction::*;
+
     let content = fs::read_to_string("inputs/day_11")?;
     let matrix: Vec<Seat> = content
         .chars()
@@ -22,25 +24,25 @@ pub fn day_11() -> io::Result<()> {
     };
 
     let not_occupied = |index: usize, matrix: &[Seat]| {
-        (seat_not_occupied(index as i32 + 1, matrix))
-            && (seat_not_occupied(index as i32 + 99, matrix))
-            && (seat_not_occupied(index as i32 - 1, matrix))
-            && (seat_not_occupied(index as i32 - 99, matrix))
-            && (seat_not_occupied(index as i32 + 100, matrix))
-            && (seat_not_occupied(index as i32 + 98, matrix))
-            && (seat_not_occupied(index as i32 - 100, matrix))
-            && (seat_not_occupied(index as i32 - 98, matrix))
+        (seat_not_occupied(index + &RIGHT, matrix))
+            && (seat_not_occupied(index + &DOWN, matrix))
+            && (seat_not_occupied(index + &LEFT, matrix))
+            && (seat_not_occupied(index + &UP, matrix))
+            && (seat_not_occupied(index + &DOWN_RIGHT, matrix))
+            && (seat_not_occupied(index + &DOWN_LEFT, matrix))
+            && (seat_not_occupied(index + &UP_RIGHT, matrix))
+            && (seat_not_occupied(index + &UP_LEFT, matrix))
     };
 
     let crowded_1 = |index: usize, matrix: &[Seat]| {
-        (!seat_not_occupied((index) as i32 + 1, matrix)) as u8
-            + (!seat_not_occupied(index as i32 + 99, matrix)) as u8
-            + (!seat_not_occupied(index as i32 - 1, matrix)) as u8
-            + (!seat_not_occupied(index as i32 - 99, matrix)) as u8
-            + (!seat_not_occupied(index as i32 + 100, matrix)) as u8
-            + (!seat_not_occupied(index as i32 + 98, matrix)) as u8
-            + (!seat_not_occupied(index as i32 - 100, matrix)) as u8
-            + (!seat_not_occupied(index as i32 - 98, matrix)) as u8
+        (!seat_not_occupied(index + &RIGHT, matrix)) as u8
+            + (!seat_not_occupied(index + &DOWN, matrix)) as u8
+            + (!seat_not_occupied(index + &LEFT, matrix)) as u8
+            + (!seat_not_occupied(index + &UP, matrix)) as u8
+            + (!seat_not_occupied(index + &DOWN_RIGHT, matrix)) as u8
+            + (!seat_not_occupied(index + &DOWN_LEFT, matrix)) as u8
+            + (!seat_not_occupied(index + &UP_RIGHT, matrix)) as u8
+            + (!seat_not_occupied(index + &UP_LEFT, matrix)) as u8
             >= 4
     };
 
@@ -52,41 +54,44 @@ pub fn day_11() -> io::Result<()> {
         });
 
     // Part 2
-    let seat_not_occupied_direction = |mut index: i32, matrix: &[Seat], direction: Direction| loop {
-        index += i32::from(&direction);
-        if is_overflow(index) {
-            break true;
-        } else {
-            match matrix[index as usize] {
-                Seat::FREE => break true,
-                Seat::OCCUPIED => break false,
-                Seat::ENDLINE => break true,
-                _ => (),
+    let seat_not_occupied_2 = |index: usize, matrix: &[Seat], direction: Direction| {
+        let mut acc: i32 = index as i32;
+        loop {
+            acc = acc as usize + &direction;
+            if is_overflow(acc) {
+                break true;
+            } else {
+                match matrix[index as usize] {
+                    Seat::FREE => break true,
+                    Seat::OCCUPIED => break false,
+                    Seat::ENDLINE => break true,
+                    _ => (),
+                }
             }
         }
     };
 
     let crowded_2 = |index: usize, matrix: &[Seat]| {
-        (!(seat_not_occupied_direction(index as i32, matrix, Direction::UP)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::UP_LEFT)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::UP_RIGHT)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::DOWN)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::DOWN_LEFT)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::DOWN_RIGHT)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::LEFT)) as u8)
-            + (!(seat_not_occupied_direction(index as i32, matrix, Direction::RIGHT)) as u8)
+        (!(seat_not_occupied_2(index, matrix, UP)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, UP_LEFT)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, UP_RIGHT)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, DOWN)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, DOWN_LEFT)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, DOWN_RIGHT)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, LEFT)) as u8)
+            + (!(seat_not_occupied_2(index, matrix, RIGHT)) as u8)
             >= 5
     };
 
     let not_occupied_2 = |index: usize, matrix: &[Seat]| {
-        (seat_not_occupied_direction(index as i32, matrix, Direction::UP))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::UP_LEFT))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::UP_RIGHT))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::DOWN))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::DOWN_LEFT))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::DOWN_RIGHT))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::LEFT))
-            && (seat_not_occupied_direction(index as i32, matrix, Direction::RIGHT))
+        (seat_not_occupied_2(index, matrix, UP))
+            && (seat_not_occupied_2(index, matrix, UP_LEFT))
+            && (seat_not_occupied_2(index, matrix, UP_RIGHT))
+            && (seat_not_occupied_2(index, matrix, DOWN))
+            && (seat_not_occupied_2(index, matrix, DOWN_LEFT))
+            && (seat_not_occupied_2(index, matrix, DOWN_RIGHT))
+            && (seat_not_occupied_2(index, matrix, LEFT))
+            && (seat_not_occupied_2(index, matrix, RIGHT))
     };
 
     let iteration_2: Box<dyn Fn((usize, &Seat, &[Seat])) -> Seat> =
@@ -101,14 +106,14 @@ pub fn day_11() -> io::Result<()> {
             .iter()
             .enumerate()
             .map(|(i, s)| iteration((i, s, &matrix)))
-            .collect::<Vec<_>>();
+            .collect();
 
         let not_changing = loop {
             let new_iter: Vec<Seat> = prev_iter
                 .iter()
                 .enumerate()
                 .map(|(i, seat)| iteration((i, seat, &prev_iter)))
-                .collect::<Vec<_>>();
+                .collect();
             if prev_iter == new_iter {
                 break new_iter;
             } else {
@@ -149,17 +154,18 @@ enum Direction {
     UP_RIGHT,
 }
 
-impl From<&Direction> for i32 {
-    fn from(item: &Direction) -> Self {
-        match item {
-            Direction::UP => -99,
-            Direction::UP_LEFT => -100,
-            Direction::UP_RIGHT => -98,
-            Direction::DOWN => 99,
-            Direction::DOWN_LEFT => 98,
-            Direction::DOWN_RIGHT => 100,
-            Direction::LEFT => -1,
-            Direction::RIGHT => 1,
+impl std::ops::Add<&Direction> for usize {
+    type Output = i32;
+    fn add(self, rhs: &Direction) -> i32 {
+        match rhs {
+            Direction::UP => self as i32 - 99,
+            Direction::UP_LEFT => self as i32 - 100,
+            Direction::UP_RIGHT => self as i32 - 98,
+            Direction::DOWN => self as i32 + 99,
+            Direction::DOWN_LEFT => self as i32 + 98,
+            Direction::DOWN_RIGHT => self as i32 + 100,
+            Direction::LEFT => self as i32 - 1,
+            Direction::RIGHT => self as i32 + 1,
         }
     }
 }
